074a69c0441f75a6d88155796c90627557d96cd6
Al1999 <aya9@pdx.edu>
2022-10-29 11:42:12 -0700

    adding new code

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': '360px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
